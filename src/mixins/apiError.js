77b441423cf6e67e9c23c74836450c507e188c87
Juan Manuel Villavicencio <juan.mvillavicencio@gmail.com>
2019-11-27 11:53:29 -0300

    add apierror handler for bad authentication

--- src/mixins/apiError.js
+++ src/mixins/apiError.js
@@ -1,11 +1,13 @@
 const errors = {
   INVALID_TRANSACTION_NO_FUNDS: 'No funds in your account',
+  INVALID_USER: 'Invalid user authentication',
 };
 
 export default {
   methods: {
     handleAPIError(type) {
-      return errors[type];
+      const errorText = errors[type];
+      return errorText || type;
     },
   },
 };
